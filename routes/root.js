61bb461417129910e6a9b8a5ec00d8f7c4679d40
LazySage01 <113522906+LazySage01@users.noreply.github.com>
2023-03-30 17:24:00 +0530

    initial commit

---
+++ routes/root.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const { dirname } = require("path");
+const router = express.Router();
+const path = require("path");
+
+router.get("^/$|/index(.html)?", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "views", "index.html"));
+});
+
+module.exports = router;
